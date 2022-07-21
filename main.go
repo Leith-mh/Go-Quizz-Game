7fda2686b6ea808d6ec4c00d4473c3563e7b592b
leith <leith.mhf@gmail.com>
2022-07-21 18:54:23 +0100

    basic score system

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
@@ -12,8 +13,48 @@ func main() {
 
 	file, err := os.Open(*csvFilename)
 	if err != nil {
-		fmt.Printf("Failed to open the CVS file: %s", *csvFilename)
-		os.Exit(1)
+		exit(fmt.Sprintf("Failed to open the CVS file: %s", *csvFilename))
 	}
-	_ = file
+
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+	problems := parseLine(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+	fmt.Printf("Your Score is %d out of possible %d. \n", correct, len(problems))
+}
+
+func parseLine(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
